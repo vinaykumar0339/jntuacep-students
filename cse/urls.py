aa128fbee310cdef291e79e32a65128d7fa9affc
AshoKumaReddy <39928813+AshokReddy01@users.noreply.github.com>
2020-10-22 10:22:43 +0530

    see

--- cse/urls.py
+++ cse/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('',cse,name='cse'),
     path('<str:regulation>/',cseRegulation,name='cse-regulation'),
     path('<str:regulation>/<int:year>/',cseYear,name='cse-year'),
+    
     path('<str:regulation>/<int:year>/<int:semester>',cseSemester,name='cse-semester'),
 ]
